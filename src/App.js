daa4836aa0906098c457881b19f87f1d449e8969
Forrest <Forrestmillerj@gmail.com>
2020-05-18 16:28:58 -0500

    import about page as a component and set up routes for it

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles'
 import {AppProvider} from './utils/GlobalContext';
 import Home from './pages/home';
+import About from './pages/about';
 import Contact from './pages/contact'
 import Portfolio from './pages/portfolio';
 import NavBar from './components/NavBar';
@@ -18,6 +19,7 @@ function App() {
         <Router>
           <NavBar/>
           <Route exact path ={[process.env.PUBLIC_URL + "/", process.env.PUBLIC_URL + "/home"]} component={Home}/>
+          <Route exact path ={process.env.PUBLIC_URL + "/about"} component={About}/>
           <Route exact path ={process.env.PUBLIC_URL + "/portfolio"} component={Portfolio}/>
         </Router>
         <SimpleBottomNavigation/>
